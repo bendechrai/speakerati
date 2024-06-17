f405e3fcb1928bab9f6cc250b28a387c4831df42
Ben Dechrai <ben@dechrai.com>
2024-06-16 21:55:59 -0500

    metadata and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,23 @@ export default function Home() {
           priority
         />
       </div>
-      <p>Welcome to the <em>Speakerati Speaker Status Program</em>!</p>
-      <p>A pioneering recognition system for frequent conference speakers, as a member of the Speakerati, your journey is celebrated with progressively prestigious levels.</p>
-      <p>Starting at Radon, you&apos;ll advance through stages like Tritium, Technetium, and Cobalt, culminating in elite levels such as Polonium, Plutonium, and the ultimate Uranium. Each level acknowledges your growing influence and expertise, offering exclusive perks, networking opportunities, and prime visibility at top events. Embrace your radiance and let your impact be felt far and wide.</p>
-<SpeakeratiMatrix />
+      <h2>
+        Welcome to the <em>Speakerati Speaker Status Program</em>!
+      </h2>
+      <p>
+        A pioneering recognition system for frequent conference speakers, as a
+        member of the Speakerati, your journey is celebrated with progressively
+        prestigious levels.
+      </p>
+      <p>
+        Starting at Radon, you&apos;ll advance through stages like Tritium,
+        Technetium, and Cobalt, culminating in elite levels such as Polonium,
+        Plutonium, and the ultimate Uranium. Each level acknowledges your
+        growing influence and expertise, offering exclusive perks, networking
+        opportunities, and prime visibility at top events. Embrace your radiance
+        and let your impact be felt far and wide.
+      </p>
+      <SpeakeratiMatrix />
     </main>
   );
 }
